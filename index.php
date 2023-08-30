ef1e0b2ec70f194aab4c53e5a60a56d816f243f9
Ezekiel Adetoro <adetorodev19@gmail.com>
2023-08-30 02:49:58 -0500

    Constants, Variables, Data Types and Operators

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
 <?php
 
-include 'include/heading.html;
+// include_once 'include/heading.html';
+// include_once 'include/heading.html';
 
-echo "Today is Friday, we\'re going to the zoo.";
 
+// echo 'Today is Friday, we\'re going to the zoo.';
+// header('Location: http://example.com');
+// As the names suggest, the include_once and require_once
+// statements only “copy and paste” the code into the file once
+// Constants, Variables, Data Types
+// and Operators
+// define ('BASIC_NUMBER', 849586586);
+// echo BASIC_NUMBER
 
+$x = 7;
+echo $x;
+
+var_dump($x); // to verify the data type of a variable
+# Type Casting
+# $p = (int)4.6;
+# echo var_dump($p);
+$x += 5;
+echo $x;
+
+$p = 3;
+echo $p;
+echo ++$p;
 ?>
